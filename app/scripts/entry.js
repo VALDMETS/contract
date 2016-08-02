1baff2870433483b075a5a71648d8515fbf91d44
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-08-01 23:28:41 -0500

    best version

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import router from './components/router';
 
+location.hash = '/main';
 ReactDOM.render(router, document.getElementById('container'));
